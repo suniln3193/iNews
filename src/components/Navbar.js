fc842a19fa42b3af05ca38ab5a88059b692485a9
suniln3193 <suniln3193@gmail.com>
2024-08-01 00:02:35 +0530

    Dark and Light mode implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,31 @@
 import React, { useState } from 'react'
 import dayIco from '../img/day.svg';
-import nightIco from '../img/night.svg';
+import nightIco from '../img/moon.png';
 
 
 const Navbar = (props) => {
 
     // Dark Mode Enable
-    const [mode, setMode] = useState('dark')
-    const [modeText, setModeText] = useState('Dark Mode')
-    const [modeImg, setModeImg] = useState(`${props.dayIco}`)
-
+    const [isDay, setIsDay] = useState(true);
+    const [isDayTxt, setIsDayTxt] = useState('Switch to Light Mode');
     const toggleMode = () => {
-        if( mode === 'dark'){
-            setMode('light');
-            setModeText('Light Mode')
-            setModeImg('../img/night.svg')
-            document.body.classList.add('dark-mode');
-            document.body.classList.remove('light-mode');
-            
-        }else{
-            setMode('dark');
-            setModeText('Dark Mode')
-            setModeImg('dayIco')
-            document.body.classList.add('light-mode');
-            document.body.classList.remove('dark-mode');
-        }
+        isDay ? setIsDayTxt('Switch to Light Mode') : setIsDayTxt('Switch to Dark Mode')
+        setIsDay(!isDay);
+        document.body.classList.toggle('dark-mode');
+        document.body.classList.toggle('light-mode');
     }
 
     
     return(
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark">
+            <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">iNews</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 pt-3 pt-md-0">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 pt-3 pt-md-0 mt-lg-1">
                             <li className="nav-item px-md-2">
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
@@ -47,8 +35,9 @@ const Navbar = (props) => {
                         </ul>
                         
                         {/* Dark mode switch */}
-                        <div className='mode' onClick={toggleMode}>
-                            <img className='modeIco' src={dayIco} ></img>
+                        <div className='mode position-relative' onClick={toggleMode}>
+                            <img className='modeIco' src={isDay ? nightIco : dayIco} alt=''></img>
+                            <div className='modeChangeAlert'>{isDayTxt}</div>
                         </div>
                     </div>
                 </div>
