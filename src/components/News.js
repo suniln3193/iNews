a9a43e332013e7fc382ee879877f45eeccb05a43
suniln3193 <suniln3193@gmail.com>
2024-08-08 00:23:03 +0530

    Badge added

--- src/components/News.js
+++ src/components/News.js
@@ -16,9 +16,11 @@ export default class News extends Component {
     pageSize: PropTypes.number,
     category: PropTypes.string,
   };
+
   capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -26,15 +28,15 @@ export default class News extends Component {
       loading: true,
       page: 1,
       totalResults: 0,
+      hasMore: true,
     };
-    document.title = `${this.capitalizeFirstLetter(
-      this.props.category
-    )} - iNews`;
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - iNews`;
   }
 
   async updateNews() {
     this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    // const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://saurav.tech/NewsAPI/top-headlines/category/${this.props.category}/${this.props.country}.json`
     this.setState({ loading: true });
     let data = await fetch(url);
     this.props.setProgress(30);
@@ -44,53 +46,50 @@ export default class News extends Component {
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
+      hasMore: parsedData.articles.length > 0,
     });
     this.props.setProgress(100);
   }
+
   async componentDidMount() {
     this.updateNews();
   }
 
-  handlePrevClick = async () => {
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews();
-  };
-
-  handleNextClick = async () => {
-    this.setState({ page: this.state.page + 1 });
-    this.updateNews();
-  };
-
   fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    if (this.state.loading || !this.state.hasMore) return;
+
+    this.setState({ loading: true });
+    // const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    let url = `https://saurav.tech/NewsAPI/top-headlines/category/${this.props.category}/${this.props.country}.json`
     let data = await fetch(url);
     let parsedData = await data.json();
+
     this.setState({
       articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
+      page: this.state.page + 1,
+      loading: false,
+      hasMore: parsedData.articles.length >= this.props.pageSize,
     });
   };
 
   render() {
     return (
       <>
-      <InfiniteScroll
-            dataLength={this.state.articles.length}
-            next={this.fetchMoreData}
-            hasMore={this.state.articles.length !== this.state.totalResults}
-            loader={<Spinner />}
-          >
-        <div className="news_wrapper">
-          <h1 className="news_heading text-center pt-2 pb-5 heading">
-            <span>iNews</span> - Top{" "}
-            <span className="text-capitalize categroyName heading">
-              {this.props.category}
-            </span>{" "}
-            Headlines{" "}
-          </h1>
-          {this.state.loading && <Spinner />}
-          
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={<Spinner />}
+        >
+          <div className="news_wrapper">
+            <h1 className="news_heading text-center pt-2 pb-5 heading">
+              <span>iNews</span> - Top{" "}
+              <span className="text-capitalize categroyName heading">
+                {this.props.category}
+              </span>{" "}
+              Headlines
+            </h1>
+            {this.state.loading && !this.state.hasMore && <Spinner />}
             <div className="container">
               <div className="row">
                 {this.state.articles.map((element) => {
@@ -101,13 +100,14 @@ export default class News extends Component {
                       title={element.title}
                       description={element.description}
                       url={element.url}
+                      author={element.author}
+                      
                     />
                   );
                 })}
               </div>
             </div>
-         
-        </div>
+          </div>
         </InfiniteScroll>
       </>
     );
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -5,19 +5,34 @@ import placeholder from '../img/placeholder.jpg'
 export default class NewsItems extends Component {
 
   render() {
-    let {title, description, urlToImage, url} = this.props;
+    let {title, description, urlToImage, url, author} = this.props;
 
     return (
         <div className='col-md-3'>
             <div className='newsItems w-100'>
                 <div className='newsItemsInner'>
                     <div className="card border-0">
-                        <img src={urlToImage ? urlToImage : placeholder} className="card-img-top" alt={title} />
+                    <div className='img_placeholder position-relative'>
+                        <img 
+                            src={urlToImage ? urlToImage : placeholder} 
+                            className="card-img-top" 
+                            alt={title} 
+                        />
+                        {author && (
+                            <span 
+                            className="badge bg-warning text-dark position-absolute top-0 left-0 d-block p-1 border-0" 
+                            style={{ backgroundColor: 'var(--redTheme)' }}
+                            >
+                            {author}
+                            </span>
+                        )}
+                        </div>
                         <div className="card-body">
                             <h5 className="card_heading card-title heading">{title}</h5>
                             {/* <p className="card-text">{description}</p> */}
                             <p className="card-text">{description}</p>
                             <a href={url} className="btn btn-primary">Read More</a>
+
                         </div>
                     </div>
                 </div>
